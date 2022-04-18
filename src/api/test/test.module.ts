4eedf3672f7577a9397667d21bb71ac47e5bc503
jinxin <jinxin@hsmap.com>
2022-04-18 17:04:39 +0800

    feat: 日志系统添加

--- src/api/test/test.module.ts
+++ src/api/test/test.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TestService } from './test.service';
 import { TestController } from './test.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Test } from './entities/test.entity';
+import { logger } from '../../middleware/logger.middleware';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Test])],
   controllers: [TestController],
-  providers: [TestService]
+  providers: [TestService],
 })
-export class TestModule {}
+export class TestModule implements NestModule {
+  // 使用中间件
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(logger).forRoutes(TestController);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
 import { AppModule } from './application.module';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
+import { logger } from './middleware/logger.middleware';
 
 async function bootstrap() {
-  const server = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{ logger: console });
 
-  await server.listen(3000);
+  // 日志系统同时解决参数日志打印问题
+  app.use(express.json({ limit: '50mb' })); // For parsing application/json
+  app.use(express.urlencoded({ extended: true, limit: '50mb' })); // For parsing application/x-www-form-urlencoded
+
+  await app.listen(3000);
 }
 
 bootstrap();
